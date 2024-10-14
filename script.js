b0cfcb641c250bc0198dd7286e3281952d1db780
JYOTIPATIL-hub <142783664+JYOTIPATIL-hub@users.noreply.github.com>
2024-10-14 20:54:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
 //your JS code here. If required.
- const squares = document.querySelectorAll('.square');
-
-        squares.forEach((square) => {
-            square.addEventListener('mouseover', () => {
-                // Change background color on hover
-                square.style.backgroundColor = '#00f'; // New hover color
-                // Revert back to initial color after 1 second
-                setTimeout(() => {
-                    square.style.backgroundColor = '#ccc'; // Initial color
-                }, 1000);
-            });
-        });
